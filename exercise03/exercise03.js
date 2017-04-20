299e6cc0e246d11968ede17370c01dd749b25c17
jonnyps <jonny_spry@msn.com>
2017-04-20 23:18:34 +0100

    complete exercise03

--- exercise03/exercise03.js
+++ exercise03/exercise03.js
@@ -1,16 +1,5 @@
-// Add your custom js here
-
-// $('.RevealCard-header').click( function() {
-//   console.log('hello');
-//   $(this).css({backgroundColor:'red'})
-// })
-
-$( ".RevealCard-header" ).click(function() {
+$(".RevealCard-header").click(function() {
   console.log('clicked')
-  $(this).next('.RevealCard-list').toggle( "slow", function() {
-    //Animation complete.
-  });
-  // $( ".RevealCard-list" ).toggle( "slow", function() {
-    // Animation complete.
-  // });
+  $(this).next('.RevealCard-list').toggle( "slow");
+  $(this).toggleClass('changed');
 });
