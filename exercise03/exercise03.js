2200d3eb3351ce45c7a0b5b83f90961a144c67e5
jonnyps <jonny_spry@msn.com>
2017-04-21 08:31:26 +0100

    add comments into jquery for exercise 03

--- exercise03/exercise03.js
+++ exercise03/exercise03.js
@@ -1,5 +1,6 @@
 $(".RevealCard-header").click(function() {
-  console.log('clicked')
+  // seek the next element with the RevealCard-list class name, and toggle it
   $(this).next('.RevealCard-list').toggle( "slow");
+  // apply class to toggled element which changes the content attribute to display plus sign
   $(this).toggleClass('changed');
 });
